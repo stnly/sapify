8b5e0f51039ef1da7cef8725eba6e47b2809da44
Stanley Tan <me@stnly.com>
2012-12-09 14:29:52 +1100

    Add 2 tests for individual user id

--- api_server_test.rb
+++ api_server_test.rb
@@ -11,8 +11,21 @@ describe 'The REST API server' do
     Sinatra::Application
   end
   
-  it "returns all users" do
+  it "returns all user ids" do
     get '/api/users/'
     last_response.should be_ok
-  end  
+    last_response.body.should == '{"status":"ok","users":["1","2"]}'
+  end
+
+  it 'returns no user details' do
+    get '/api/users/100'
+    last_response.should be_ok
+    last_response.body.should == '{"status":"error","id":"none","name":"none"}'
+  end
+
+  it 'returns user details' do
+    get '/api/users/1'
+    last_response.should be_ok
+    last_response.body.should == '{"status":"ok","id":"1","name":"John"}'
+  end
 end
